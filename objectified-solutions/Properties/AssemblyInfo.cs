f4b9b8f93e71baaddc09e52e10f69e013c777dd1
Andrew <andrew.paul.gray@gmail.com>
2013-03-31 13:08:49 +1100

    now its a nuget package on nuget.org

--- objectified-solutions/Properties/AssemblyInfo.cs
+++ objectified-solutions/Properties/AssemblyInfo.cs
@@ -2,26 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("objectified-solutions")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Oakton Ltd")]
-[assembly: AssemblyProduct("objectified-solutions")]
-[assembly: AssemblyCopyright("Copyright © Oakton Ltd 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("50fd5e9f-24eb-4231-ae9c-16cb05d52c41")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -33,4 +13,24 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitleAttribute("objectified-solutions")]
+[assembly: AssemblyDescriptionAttribute("Solution File Object Modeller. objectified-solutions produces an navigatable object graph of an Visual Studio solution (.sln file) and subordinate project(s) (.csproj file(s)).")]
+[assembly: AssemblyConfigurationAttribute("")]
+[assembly: AssemblyCompanyAttribute("Phoenix Consulting Pty Ltd")]
+[assembly: AssemblyProductAttribute("objectified-solutions")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Phoenix Consulting Pty Ltd 2013")]
+[assembly: AssemblyTrademarkAttribute("")]
+[assembly: AssemblyCultureAttribute("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50fd5e9f-24eb-4231-ae9c-16cb05d52c41")]
